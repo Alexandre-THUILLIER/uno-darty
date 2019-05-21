f42b3d125420165c69a8dd7d656bf827d835e0ab
Alexandre THUILLIER <alexandrethuillier@icloud.com>
2019-05-21 23:49:33 +0200

    nouvelle gestion des joueurs

--- js/script-uno.js
+++ js/script-uno.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 
     players = [];
-
-    userInfo = [];
+    idUserAuto = 0;
 
     updateOrder();
+
+    function idAleatoire(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 
     // Ajout d'un user dans le tableau
     $('.add-gamer button').click(function(e) {
@@ -14,14 +17,27 @@ $(document).ready(function() {
 
         if ( name != '' ) {
 
-            players.push(name);
+            idUserAuto = idUserAuto + 1;
+
+            players.push(
+                {
+                    id: idUserAuto,
+                    name: name,
+                    totalScore: 0,
+                    gameWin: 0,
+                    lastScore: 0,
+                    history: ''
+                }
+            );
 
             $('.list-gamer .content').html('');
             for (i = 0; i < players.length; ++i) {
-                $('.list-gamer .content').append('<div class="player"><button class="delete" data-key="' + i + '"><i class="icon-cancel-circled"></i></button><span>' + players[i] + '</span></div>')
+                $('.list-gamer .content').append('<div class="player"><button class="delete" data-key="' + players[i].id + '"><i class="icon-cancel-circled"></i></button><span>' + players[i].name + '</span></div>')
             }
             $('.error').text('');
             $(this).prev('input').val('');
+
+            console.log(players);
 
             // Suppression d'un user de la liste
             deleteUserList();
@@ -39,39 +55,42 @@ $(document).ready(function() {
 
         if ( $('#begin-play .content').hasClass('selected') ) {
 
-            for ( s = 0; s < sessionStorage.length; s++) {
+            for ( s = 0; s < players.length; s++) {
 
-                if ( $('.user[data-user="'+s+'"] input[type="tel"]').val() !== "" ) {
-                    var newScore = parseFloat($('.user[data-user="'+s+'"] input[type="tel"]').val()); // valeur du champ input
-                } else if ( $('.user[data-user="'+s+'"]').hasClass('the-winner') ) {
-                    var newScore = -20;
-                } else {
-                    var newScore = 0;
+                const dataUser = $('.user[data-user="' + players[s].id + '"]');
+
+                var newScore = 0;
+                if ( dataUser.find('input[type="tel"]').val() !== "" ) {
+                    newScore = parseFloat(dataUser.find('input[type="tel"]').val()); // valeur du champ input
+                } else if ( $('.user[data-user="' + players[s].id + '"]').hasClass('the-winner') ) {
+                    newScore = -20;
                 }
 
-                var actuallyScore = parseFloat(sessionStorage.getItem(sessionStorage.key(s)));
+                players[s].lastScore = newScore;
+
+                var actuallyScore = players[s].totalScore;
 
                 // SCORE FINAL
-                var score = newScore + actuallyScore;
+                var finalScore = newScore + actuallyScore;
+                players[s].totalScore = finalScore;
 
-                sessionStorage.setItem(sessionStorage.key(s), score);
+                if (players[s].history === '') {
+                    players[s].history = newScore.toString();
+                } else {
+                    players[s].history = players[s].history + ' / ' + newScore;
+                }
 
-                $('.user[data-user="'+s+'"] .points span').text(parseFloat(sessionStorage.getItem(sessionStorage.key(s))));
-                $('.user[data-user="'+s+'"]').attr('data-score', score);
+                dataUser.find('.points span').text(parseFloat(finalScore));
+                dataUser.attr('data-score', finalScore);
 
                 // Ajout de marque au vainqueur
-                if ( $('.user[data-user="'+s+'"]').hasClass('the-winner') ) {
-                    $('.user[data-user="'+s+'"] .winner').append('<span></span>');
+                if ( dataUser.hasClass('the-winner') ) {
+                    players[s].gameWin = players[s].gameWin + 1;
+                    dataUser.find('.winner').append('<span></span>');
                 }
 
                 // Ajout de l'historique des points
-                var historique = $('.user[data-user="'+s+'"] .historique').html();
-                if ( historique == "") {
-                    historique = '<span>' + newScore + '</span>';
-                } else {
-                    historique += ' / <span>' + newScore + '</span>';
-                }
-                $('.user[data-user="'+s+'"] .historique').html(historique);
+                dataUser.find('.historique').html(players[s].history);
 
             }
 
@@ -90,22 +109,28 @@ $(document).ready(function() {
     $('.maj').click(function(e) {
         e.preventDefault();
 
-        for ( f = 0; f < sessionStorage.length; f++) {
+        for ( i = 0; i < players.length; i++) {
 
-            if ( $('.user[data-user="'+f+'"] input[type="tel"]').val() !== "" ) {
-                var newScore = parseFloat($('.user[data-user="'+f+'"] input[type="tel"]').val()); // valeur du champ input
-            } else {
-                var newScore = 0;
+            const dataUser = $('.user[data-user="' + players[i].id + '"]');
+            const actuallyScore = parseFloat(players[i].totalScore);
+            const lastScore = parseFloat(players[i].lastScore);
+
+            let updateScore = lastScore;
+            if ( dataUser.find('input[type="tel"]').val() !== "" ) {
+                updateScore = parseFloat(dataUser.find('input[type="tel"]').val()); // valeur du champ input
             }
+            players[i].lastScore = updateScore;
 
-            var actuallyScore = parseFloat(sessionStorage.getItem(sessionStorage.key(f)));
-            var score = newScore + actuallyScore;
-            sessionStorage.setItem(sessionStorage.key(f), score);
+            const score = updateScore + (actuallyScore - lastScore);
 
-            $('.user[data-user="'+f+'"] .points span').text(parseFloat(sessionStorage.getItem(sessionStorage.key(f))));
+            players[i].totalScore = score;
+
+            dataUser.find('.points span').text(score);
 
             // Correction du dernier score en historique
-            $('.user[data-user="'+f+'"] .historique span:last-child').text(score);
+            const indexOfSlash = players[i].history.lastIndexOf('/');
+            players[i].history = players[i].history.substring(0, indexOfSlash) + '/ ' + updateScore;
+            dataUser.find('.historique').text(players[i].history);
 
         }
 
@@ -133,21 +158,17 @@ $(document).ready(function() {
 
         sessionStorage.clear();
 
-        localStorage.setItem('nb_playeurs', players.length);
+        console.log(players);
 
-        $.each(players, function(index, value) {
-            sessionStorage.setItem(value, 0);
-        });
+        $('#begin-play h4 span').text(players.length);
 
-        if ( $('.list-gamer .content').html() !== "" ) {
+        if ( players.length > 0 ) {
 
             $('#begin-play').show();
 
-            for ( e = 0; e < sessionStorage.length; e++) {
+            for ( e = 0; e < players.length; e++) {
 
-                var keyName = sessionStorage.key(e);
-
-                $('#begin-play > .content').append('<div class="user" data-user="'+ e +'" data-score="0" data-name="'+ keyName +'"><div class="win"><i class="icon-crown"></i></div><div class="name">'+ sessionStorage.key(e) +'<span class="winner"></span></div><div class="points"><input type="tel" /><span>'+ sessionStorage.getItem(sessionStorage.key(e)) +'</span> pts</div><div class="historique"></div></div>');
+                $('#begin-play > .content').append('<div class="user" data-user="'+ players[e].id +'" data-score="0" data-name="'+ players[e].name +'"><div class="win"><i class="icon-crown"></i></div><div class="name">'+ players[e].name +'<span class="winner"></span></div><div class="points"><input type="tel" /><span>'+ players[e].totalScore +'</span> pts</div><div class="historique"></div></div>');
             }
 
             $('.error').text('');
@@ -174,27 +195,49 @@ $(document).ready(function() {
         $('.new_playeurs_to_added').slideToggle();
     });
 
-    addNewPlayeurInGame()
+    // addNewPlayeurInGame()
+
+    $(document).on('click', '#btn_add_new_playeur', function() {
+
+        const sortedUsers = players.sort(function (a, b) {
+            return a.totalScore - b.totalScore;
+        });
+
+        const indexLastUser = sortedUsers.length - 1;
+        const highestScore = sortedUsers[indexLastUser].totalScore + 1;
+        const name = $('.new_playeurs_to_added #firstname').val();
+
+        idUserAuto = idUserAuto + 1;
+
+        players.push(
+            {
+                id: idUserAuto,
+                name: name,
+                totalScore: highestScore,
+                gameWin: 0,
+                lastScore: highestScore,
+                history: highestScore.toString()
+            }
+        );
+
+        $('#begin-play > .content').append('<div class="user" data-user="'+ idUserAuto +'" data-score="0" data-name="'+ name +'"><div class="win"><i class="icon-crown"></i></div><div class="name">'+ name +'<span class="winner"></span></div><div class="points"><input type="tel" /><span>'+ highestScore +'</span> pts</div><div class="historique">' + highestScore.toString() + '</div></div>');
+
+        $('.new_playeurs_to_added #firstname').val('');
+
+    });
+
 });
 
 function deleteUserList() {
 
-    $('.delete').on('click', function(e) {
+    $(document).on('click', '.delete', function(e) {
         e.preventDefault();
 
         var elementToDelete = $(this).attr('data-key');
+        players = players.filter((player) => player.id != parseFloat(elementToDelete));
 
-        players.splice(elementToDelete, 1);
+        $(this).parent().remove();
 
-        $('.list-gamer .content').html('');
-        for (i = 0; i < players.length; ++i) {
-            $('.list-gamer .content').append('<div class="player"><button class="delete" data-key="' + i + '"><i class="icon-cancel-circled"></i></button><span>' + players[i] + '</span></div>')
-        }
-
-        // Suppression d'un user de la liste
-        deleteUserList();
-
-        console.log(players);
     });
 }
 
@@ -207,7 +250,7 @@ function updateOrder() {
         divList.sort(function (a, b) {
             return  $(a).attr('data-score') - $(b).attr('data-score');
         });
-        for (i = 0; i < sessionStorage.length; i++) {
+        for (i = 0; i < players.length; i++) {
             $('<div class="sort-user"><div class="sort-classement">' + (i+1) + '<span>e</span></div><div class="sort-pseudo">' + divList.eq(i).attr('data-name') + '</div><div class="sort-score">' + divList.eq(i).attr('data-score') + ' points</div></div>').appendTo(".sortBy .content");
         }
 
